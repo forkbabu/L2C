dc7a569c27da95c26f11448bd745f0a1f6ee24d3
Forkbabu <sayantandas30011998@gmail.com>
2020-05-31 22:42:24 +0530

    Update default.py

--- dataloaders/default.py
+++ dataloaders/default.py
@@ -3,18 +3,78 @@ import torchvision
 from torchvision import transforms
 from .sampler import RandSubClassSampler
 
+import numpy as np
 
-def IndianPines(batch_sz, num_workers=2):
-    
-    train_dataset = #create a pytorch dataset
-    train_loader = #create a loader
-    train_loader.num_classes = 16
 
-    eval_dataset = #create a pytorch dataset
-    eval_loader = #create a loader
-    eval_loader.num_classes = 16
+def index_assignment(index, row, col, pad_length):
+    new_assign = {}
+    for counter, value in enumerate(index):
+        assign_0 = value // col + pad_length
+        assign_1 = value % col + pad_length
+        new_assign[counter] = [assign_0, assign_1]
+    return new_assign
 
-    return train_loader, eval_loader
+
+def assignment_index(assign_0, assign_1, col):
+    new_index = assign_0 * col + assign_1
+    return new_index
+
+
+def select_patch(matrix, pos_row, pos_col, ex_len):
+    selected_rows = matrix[range(pos_row-ex_len, pos_row+ex_len+1)]
+    selected_patch = selected_rows[:, range(pos_col-ex_len, pos_col+ex_len+1)]
+    return selected_patch
+
+
+def select_small_cubic(data_size, data_indices, whole_data, patch_length, padded_data, dimension):
+    small_cubic_data = np.zeros((data_size, 2 * patch_length + 1, 2 * patch_length + 1, dimension))
+    data_assign = index_assignment(data_indices, whole_data.shape[0], whole_data.shape[1], patch_length)
+    for i in range(len(data_assign)):
+        small_cubic_data[i] = select_patch(padded_data, data_assign[i][0], data_assign[i][1], patch_length)
+    return small_cubic_data
+
+def IndianPines(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE, total_indices, VAL_SIZE,
+                  whole_data, PATCH_LENGTH, padded_data, INPUT_DIMENSION, batch_size, gt):
+
+    gt_all = gt[total_indices] - 1
+    y_train = gt[train_indices] - 1
+    y_test = gt[test_indices] - 1
+
+
+    train_data = select_small_cubic(TRAIN_SIZE, train_indices, whole_data,
+                                                        PATCH_LENGTH, padded_data, INPUT_DIMENSION)
+    test_data = select_small_cubic(TEST_SIZE, test_indices, whole_data,
+                                                       PATCH_LENGTH, padded_data, INPUT_DIMENSION)
+    x_train = train_data.reshape(train_data.shape[0], train_data.shape[1], train_data.shape[2], INPUT_DIMENSION)
+    x_test_all = test_data.reshape(test_data.shape[0], test_data.shape[1], test_data.shape[2], INPUT_DIMENSION)
+
+    x_test = x_test_all[:-VAL_SIZE]
+    y_test = y_test[:-VAL_SIZE]
+
+    x1_tensor_train = torch.from_numpy(x_train).type(torch.FloatTensor).unsqueeze(1)
+    y1_tensor_train = torch.from_numpy(y_train).type(torch.FloatTensor)
+    torch_dataset_train = Data.TensorDataset(x1_tensor_train, y1_tensor_train)
+
+    x1_tensor_test = torch.from_numpy(x_test).type(torch.FloatTensor).unsqueeze(1)
+    y1_tensor_test = torch.from_numpy(y_test).type(torch.FloatTensor)
+    torch_dataset_test = Data.TensorDataset(x1_tensor_test,y1_tensor_test)
+
+
+    train_iter = Data.DataLoader(
+        dataset=torch_dataset_train,  # torch TensorDataset format
+        batch_size=batch_size,  # mini batch size
+        shuffle=True,  # 要不要打乱数据 (打乱比较好)
+        num_workers=0,  # 多线程来读数据
+    )
+    test_iter = Data.DataLoader(
+        dataset=torch_dataset_test,  # torch TensorDataset format
+        batch_size=batch_size,  # mini batch size
+        shuffle=False,  # 要不要打乱数据 (打乱比较好)
+        num_workers=0,  # 多线程来读数据
+    )
+    return train_iter, test_iter
+
+
 
 
 
